d1a37599793f9488166d29908b5306729643e18d
kianyangchn <kianyangchn@gmail.com>
2018-03-04 09:28:05 +0800

    fix bug

--- min_rotated_array.py
+++ min_rotated_array.py
@@ -2,7 +2,7 @@
 # @Author: kianyangchn
 # @Date:   2018-03-04 09:09:47
 # @Last Modified by:   kianyangchn
-# @Last Modified time: 2018-03-04 09:21:48
+# @Last Modified time: 2018-03-04 09:27:57
 
 
 class Solution:
@@ -21,8 +21,8 @@ class Solution:
                 return nums[st]
             else:
                 if nums[mid] < nums[ed]:
-                    ed = mid - 1
-                elif: nums[mid] > nums[st]
-                    st = mid + 1
-                elif: nums[mid] == nums[st]:
+                    ed = mid
+                elif nums[mid] > nums[st]:
+                    st = mid
+                elif nums[mid] == nums[st]:
                     return nums[ed]
